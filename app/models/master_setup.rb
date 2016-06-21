b0df09675eaf82385bd1f94356ea8a1425932bb8
Sable User <casey@sableassets.com>
2016-06-21 16:03:46 +0200

    Add validations to Master setup and Job titles

--- app/models/master_setup.rb
+++ app/models/master_setup.rb
@@ -1,4 +1,5 @@
 class MasterSetup < ActiveRecord::Base
+  validates :municipality,:province, :presence=> true, :uniqueness=>true
   has_many :regions, :dependent => :destroy
   accepts_nested_attributes_for :regions, allow_destroy: true
   def regions_for_form
