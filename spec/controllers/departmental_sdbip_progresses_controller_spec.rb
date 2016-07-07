3cf1c9842b10821c917f6479b34447c784cd42b5
Rachidi Bushidi <Rachidi@sableassets.co.za>
2016-07-07 13:40:38 +0200

    Add minor rspec fix

--- spec/controllers/departmental_sdbip_progresses_controller_spec.rb
+++ spec/controllers/departmental_sdbip_progresses_controller_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe DepartmentalSdbipProgressesController, :type => :controller do
         get :index
       end
       it {expect(response).to redirect_to(login_path)}
-      it {flash[:danger].should eq("Please log in.")}
+      it {expect(flash[:danger]).to eq("Please log in.")}
     end
 
 
