d208149148d63a189b5b2f1ecb2e89405c7833d8
Rachidi Bushidi <Rachidi@sableassets.co.za>
2016-07-13 16:15:46 +0200

    Add tool to pick up unnecessary database queries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,4 +46,26 @@ Rails.application.configure do
   config.web_console.development_only = false
 
   config.active_job.queue_adapter = :delayed_job
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.growl = false
+    #Bullet.xmpp = { :account  => 'bullets_account@jabber.org',
+    #                :password => 'bullets_password_for_jabber',
+    #                :receiver => 'your_account@jabber.org',
+    #                :show_online_status => false }
+    Bullet.rails_logger = true
+    Bullet.honeybadger = false
+    Bullet.bugsnag = false
+    Bullet.airbrake = true
+    Bullet.rollbar = false
+    Bullet.add_footer = true
+    #Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    #Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware' ]
+    #Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
+    Bullet.raise = true
+  end
 end
