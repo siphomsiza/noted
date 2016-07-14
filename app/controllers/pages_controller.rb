fa77a49512b85fec6ce06740f0daac2a4aca636d
Sable User <casey@sableassets.com>
2016-07-14 17:03:35 +0200

    Fix dashboard chart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -24,14 +24,15 @@ class PagesController < ApplicationController
     @departmental_sdbips = DepartmentalSdbip.order(performance_standard: :asc)
 
     begin
-      @client = YahooWeather::Client.new
-      @response = @client.fetch(1582504)
-      @doc = @response.doc
-      @forecast = @doc["item"]["forecast"]
+
+        @client = YahooWeather::Client.new
+        @response = @client.fetch(1582504)
+        @doc = @response.doc
+        @forecast = @doc["item"]["forecast"]
       #@response = @client.fetch_by_location('New York')
       #@response.units.temperature
       #@response.condition.temp
-
+    
   rescue SignalException => e
     flash[:notice] = "received Exception #{e.message}"
     puts "received Exception #{e}"
