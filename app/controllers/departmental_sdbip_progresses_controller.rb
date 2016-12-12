6bd1693b4f0725234ee99e3f1ff37a0ef68d8b6b
Sable User <casey@sableassets.com>
2016-12-12 12:54:56 +0200

    Fix to Test tools

--- app/controllers/departmental_sdbip_progresses_controller.rb
+++ app/controllers/departmental_sdbip_progresses_controller.rb
@@ -6,7 +6,6 @@ class DepartmentalSdbipProgressesController < ApplicationController
 
   def index
     weather_details
-    selected_headings = []
     if params[:data_value] && !params[:data_value].blank? && (params[:start_date] || params[:end_date] || params[:department_id] || params[:department_id] || params[:subdepartment_id] || params[:kpi_ref_number] || params[:predetermined_objective_id] || params[:kpi_owner_id] || params[:kpi] || params[:unit_of_measurement] || params[:mcore_classification_id] || params[:strategic_objective_id] || params[:source_of_evidence] || params[:baseline] || params[:annual_target] || params[:revised_target] || params[:national_kpa_id] || params[:ndp_objective_id] || params[:kpi_concept_id] || params[:kpi_type_id] || params[:provincial_strategic_outcome_id] || params[:ward_id] || params[:area_id] || params[:performance_standard] || params[:kpi_calculation_type_id] || params[:kpi_target_type_id])
       selected_audit_headers = params[:data_value]
       $selected_audit_headers = params[:data_value]
